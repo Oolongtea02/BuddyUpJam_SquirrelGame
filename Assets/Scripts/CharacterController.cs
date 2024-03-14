a7ffe19fbbcd87e252d906e68d5f2eaaab9c15c6
Justin <just7166@gmail.com>
2024-03-14 19:03:48 -0400

    Added character controls and Log objects

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+    float acceleration = 30.0f;
+    SpriteRenderer sprite;
+    
+    void Start()
+    {
+        //Fetch the Rigidbody from the GameObject with this script attached
+        m_Rigidbody = GetComponent<Rigidbody>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("up"))
+            {
+            m_Rigidbody.AddForce(transform.up * acceleration);
+        }
+        if (Input.GetKey("down"))
+        {
+            m_Rigidbody.AddForce(transform.up * -acceleration);
+        }
+        if (Input.GetKey("right"))
+        {
+            m_Rigidbody.AddForce(transform.right * acceleration);
+            if (!Input.GetKey("left"))
+            { sprite.flipX = false; }
+        }
+        if (Input.GetKey("left"))
+        {
+            m_Rigidbody.AddForce(transform.right * -acceleration);
+            if (!Input.GetKey("right"))
+            { sprite.flipX = true; }
+        }
+    }
+}
+
